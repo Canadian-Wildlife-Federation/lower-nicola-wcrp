a85ac927834271beebeda9013b5cc319debe0a58
andrewp-CWF <AndrewP@cwf-fcf.org>
2024-08-23 10:42:20 -0400

    Update DFO branch

---
+++ content/python/api_calls.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+import warnings
+import requests
+import json
+import pandas
+
+def barrier_extent(barrier_type):
+
+    request = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_extent/items.json?watershed_group_code=LNIC&barrier_type=' + barrier_type
+
+    response_api = requests.get(request)
+    parse = response_api.text
+    result = json.loads(parse)
+
+    blocked_km = result[0]['all_habitat_blocked_km']
+    blocked_pct = result[0]['extent_pct']
+
+    return blocked_km, blocked_pct
+
+def barrier_count(barrier_type):
+    request = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_count/items.json?watershed_group_code=LNIC&barrier_type=' + barrier_type
+
+    response_api = requests.get(request)
+    parse = response_api.text
+    result = json.loads(parse)
+
+    n_passable = result[0]['n_passable']
+    n_barrier = result[0]['n_barrier']
+    n_potential = result[0]['n_potential']
+    n_unknown = result[0]['n_unknown']
+
+    sum_bar = (n_passable, n_barrier, n_potential, n_unknown)
+
+    return n_passable, n_barrier, n_potential, n_unknown, sum(sum_bar)
+
+def barrier_severity(barrier_type):
+
+    request = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_severity/items.json?watershed_group_code=LNIC&barrier_type=' + barrier_type
+
+    response_api = requests.get(request)
+    parse = response_api.text
+    result = json.loads(parse)
+
+    n_assessed_barrier = result[0]['n_assessed_barrier']
+    n_assess_total = result[0]['n_assess_total']
+    pct_assessed_barrier = result[0]['pct_assessed_barrier']
+
+    return n_assessed_barrier, n_assess_total, pct_assessed_barrier
+
+def watershed_connectivity(habitat_type):
+
+    request = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_habitat_connectivity_status/items.json?watershed_group_code=LNIC&habitat_type=' + habitat_type
+
+    response_api = requests.get(request)
+    parse = response_api.text
+    result = json.loads(parse)
+
+    connect_stat = result[0]['connectivity_status']
+    all_habitat = result[0]['all_habitat']
+    all_habitat_acc = result[0]['all_habitat_accessible']
+
+    return round(connect_stat), all_habitat, all_habitat_acc
+
+warnings.filterwarnings('ignore')
+
+connect = watershed_connectivity("ALL")[0]
+total = watershed_connectivity("ALL")[1] #total km in LNIC
+access = watershed_connectivity("ALL")[2]
+gain = round((total*0.9)-access,2)
+
+connect_spawn = watershed_connectivity("SPAWNING")[0]
+total_spawn = watershed_connectivity("SPAWNING")[1] #total km in LNIC
+access_spawn = watershed_connectivity("SPAWNING")[2]
+gain_spawn = round((total_spawn*0.9)-access_spawn,2)
+
+connect_rear = round(watershed_connectivity("REARING")[0],2)
+total_rear = round(watershed_connectivity("REARING")[1],2) #total km in LNIC
+access_rear = round(watershed_connectivity("REARING")[2],2)
+gain_rear = round((total_rear*0.9)-access_rear,2)
